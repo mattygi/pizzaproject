822edcf9be17ab0d834c009647db25433032a27f
mattygi <matthew.gimutao81@gmail.com>
2025-04-15 19:29:27 -0700

    Update editmenuitems.js

--- pizzaApp-main/pizzaApp/static/js/editmenuitems.js
+++ pizzaApp-main/pizzaApp/static/js/editmenuitems.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const deleteButtons = document.querySelectorAll("button[value='delete']");
-
-  deleteButtons.forEach(button => {
-    button.addEventListener("click", event => {
+  document.querySelector(".container").addEventListener("click", (event) => {
+    const button = event.target.closest(".delete-btn");
+    
+    if (button) {
       if (!confirm("Are you sure you want to delete this item?")) {
         event.preventDefault(); // Prevent form submission
       }
-    });
+    }
   });
 });
